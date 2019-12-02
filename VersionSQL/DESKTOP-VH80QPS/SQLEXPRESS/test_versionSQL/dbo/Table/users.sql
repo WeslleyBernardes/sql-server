04ffa0e4051ac183a89b1ca9f517b8466fbd3385
unknown <unknown>
2019-12-02 16:07:06 -0200

    Tabela dbo.users

---
+++ VersionSQL/DESKTOP-VH80QPS/SQLEXPRESS/test_versionSQL/dbo/Table/users.sql
@@ -0,0 +1,15 @@
+/****** Object:  Table [dbo].[users]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[PrimeiroNome] [varchar](255) NOT NULL,
+	[UltimoNome] [varchar](255) NULL,
+	[Idade] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
